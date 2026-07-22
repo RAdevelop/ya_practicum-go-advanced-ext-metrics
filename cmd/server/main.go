45d4351e3b1ffcc5a564f929ebeefc8fd623e057
RA <ra@zerotech.ru>
2026-07-22 19:14:27 +0300

    - Инкремент 4   - Добавлен аргумент сервера:     - Флаг `-a=<ЗНАЧЕНИЕ>` отвечает за адрес эндпоинта HTTP-сервера (по умолчанию `localhost:8080`).

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"net/http"
 
 	"github.com/RAdevelop/ya_practicum-go-advanced-ext-metrics/internal/handler"
@@ -14,10 +15,18 @@ var metricService = service.NewMetricService(metricStorage)
 
 func main() {
 
+	srvAddress := &serverAddress{
+		host: "localhost",
+		port: 8080,
+	}
+	_ = flag.Value(srvAddress)
+	flag.Var(srvAddress, "a", `Server address pattern: "host:port"`)
+	flag.Parse()
+
 	h := handler.New(metricService)
 	r := router.New(h)
 
-	err := http.ListenAndServe(":8080", r)
+	err := http.ListenAndServe(srvAddress.String(), r)
 	if err != nil {
 		panic(err)
 	}
