9b91f0b633369d6b0c60ce1521f10fd5d5511fff
RA <ra@zerotech.ru>
2026-08-28 08:32:10 +0300

    - Инкремент 11   - добавил реализацию методов storage.go

--- internal/repository/database/storage.go
+++ internal/repository/database/storage.go
@@ -2,9 +2,11 @@ package database
 
 import (
 	"context"
+	"errors"
 	"fmt"
 
 	models "github.com/RAdevelop/ya_practicum-go-advanced-ext-metrics/internal/model"
+	"github.com/jackc/pgx/v5"
 )
 
 type Storage struct {
@@ -18,35 +20,116 @@ func NewStorage(db *DB) *Storage {
 }
 
 func (s *Storage) GaugeUpdate(ctx context.Context, name string, value float64) error {
-	return s.upsert(ctx, models.Gauge, name, value)
+	return s.upsertMetric(ctx, models.Gauge, name, value)
 }
 
 func (s *Storage) GaugeByName(ctx context.Context, name string) (float64, error) {
-	return 0.0, fmt.Errorf("GaugeByName not implemented")
+
+	if err := ctx.Err(); err != nil {
+		return 0, err
+	}
+	var value *float64
+	row := s.selectMetricRow(ctx, models.Gauge, name, "value")
+
+	err := row.Scan(&value)
+	if err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			return 0, fmt.Errorf("metric value not found for name: %s, %w", name, err)
+		}
+		return 0, fmt.Errorf("scan error: %w", err)
+	}
+
+	return *value, err
 }
 
 func (s *Storage) Gauge(ctx context.Context) map[string]float64 {
-	return make(map[string]float64) //TODO
+
+	result := make(map[string]float64)
+
+	if err := ctx.Err(); err != nil {
+		return result
+	}
+
+	rows, err := s.selectMetricsRows(ctx, models.Gauge, "value")
+	if err != nil {
+		return result
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+		var name string
+		var value float64
+		if err := rows.Scan(&name, &value); err != nil {
+			return result
+		}
+		result[name] = value
+	}
+
+	//TODO надо менять интерфейс, чтобы он возвращал карту models.Metrics и error
+	if err = rows.Err(); err != nil {
+		return result
+	}
+	return result
 }
 
 func (s *Storage) CounterAdd(ctx context.Context, name string, value int64) error {
-	return fmt.Errorf("CounterAdd not implemented")
+	return s.upsertMetric(ctx, models.Counter, name, value)
 
 }
 func (s *Storage) CounterAccumulative(ctx context.Context) map[string]int64 {
-	return make(map[string]int64) //TODO
+	result := make(map[string]int64)
+
+	if err := ctx.Err(); err != nil {
+		return result
+	}
+
+	rows, err := s.selectMetricsRows(ctx, models.Counter, "delta")
+	if err != nil {
+		return result
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var name string
+		var value int64
+		if err := rows.Scan(&name, &value); err != nil {
+			return result
+		}
+		result[name] = value
+	}
+
+	//TODO надо менять интерфейс, чтобы он возвращал карту models.Metrics и error
+	if err = rows.Err(); err != nil {
+		return result
+	}
+	return result
 }
 
 func (s *Storage) CounterAccumulativeByName(ctx context.Context, name string) (int64, error) {
-	return 0, fmt.Errorf("CounterAccumulativeByName not implemented")
+	if err := ctx.Err(); err != nil {
+		return 0, err
+	}
+	var delta *int64
+	row := s.selectMetricRow(ctx, models.Counter, name, "delta")
+
+	err := row.Scan(&delta)
+	if err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			return 0, fmt.Errorf("metric value not found for name: %s, %w", name, err)
+		}
+		return 0, fmt.Errorf("scan error: %w", err)
+	}
+
+	return *delta, err
 }
 
 func (s *Storage) Ping(ctx context.Context) error {
 	return s.DB.Ping(ctx)
 }
 
-// upsert - добавляем метрику, или обновляем ее значение
-func (s *Storage) upsert(ctx context.Context, mType string, mID string, mValue any) error {
+// upsertMetric - добавляем метрику, или обновляем ее значение
+func (s *Storage) upsertMetric(ctx context.Context, mType string, mID string, mValue any) error {
 
 	if err := ctx.Err(); err != nil {
 		return err
@@ -60,8 +143,6 @@ func (s *Storage) upsert(ctx context.Context, mType string, mID string, mValue a
 		value = &v
 	case int64:
 		delta = &v
-	default:
-		return fmt.Errorf("unsupported type: %T", mValue)
 	}
 
 	_, err := s.DB.Executor(ctx).Exec(ctx, `
@@ -77,4 +158,20 @@ func (s *Storage) upsert(ctx context.Context, mType string, mID string, mValue a
 		mID, mType, delta, value,
 	)
 	return err
+}
+
+// selectMetricRow - ищем значения для метрики
+func (s *Storage) selectMetricRow(ctx context.Context, mType string, mID string, field string) pgx.Row {
+
+	sql := `SELECT ` + field + ` FROM metric WHERE metric_id = $1 AND m_type = $2`
+
+	return s.DB.Executor(ctx).QueryRow(ctx, sql, mID, mType)
+}
+
+// selectMetricsRows - ищем значения для метрик
+func (s *Storage) selectMetricsRows(ctx context.Context, mType string, field string) (pgx.Rows, error) {
+
+	sql := `SELECT metric_id, ` + field + ` FROM metric WHERE m_type = $1`
+
+	return s.DB.Executor(ctx).Query(ctx, sql, mType)
 }
--- internal/repository/database/storage_test.go
+++ internal/repository/database/storage_test.go
@@ -19,14 +19,12 @@ var envOpts = &env.Options{
 
 var errForTransactionRollback = errors.New("return for transaction rollback")
 
-func setUpStorage(t *testing.T, ctx context.Context) *Storage {
+func setUpStorage(t *testing.T) (*Storage, context.Context) {
 
 	envDB, err := configDB.NewEnvWithOptions(envOpts)
 	assert.NoError(t, err)
 
-	//TODO del
-	//t.Log("envDB.DSN()", envDB.DSN())
-
+	ctx := context.Background()
 	db, err := NewDB(ctx, envDB)
 	assert.NoError(t, err)
 
@@ -34,22 +32,246 @@ func setUpStorage(t *testing.T, ctx context.Context) *Storage {
 		db.Close()
 	})
 
-	return NewStorage(db)
+	return NewStorage(db), ctx
 }
 
-func TestStorage_GaugeUpdate(t *testing.T) {
+func TestStorage_GaugeUpdateAndGaugeByName(t *testing.T) {
 
-	ctx := context.Background()
-	storage := setUpStorage(t, ctx)
+	type given struct {
+		mId    string
+		mValue float64
+	}
+	type want struct {
+		hasError bool
+		mValue   float64
+	}
 
-	mId := "metricNameRA"
-	mValue := 0.0
+	tests := []struct {
+		name  string
+		given given
+		want  want
+	}{
+		{
+			name: "success",
+			given: given{
+				mId:    "gauge",
+				mValue: 100.11,
+			},
+			want: want{
+				hasError: false,
+				mValue:   100.11,
+			},
+		},
+	}
 
-	err := storage.DB.RunInTransaction(ctx, func(ctx context.Context) error {
-		err := storage.GaugeUpdate(ctx, mId, mValue)
-		assert.NoError(t, err)
-		return errForTransactionRollback
-	})
+	storage, ctx := setUpStorage(t)
 
-	assert.Error(t, err)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := storage.DB.RunInTransaction(ctx, func(ctx context.Context) error {
+				err := storage.GaugeUpdate(ctx, tt.given.mId, tt.given.mValue)
+				if tt.want.hasError {
+					assert.Errorf(t, err, "gauge want: %v, given: %v", tt.want, tt.given)
+				} else {
+					assert.NoErrorf(t, err, "gauge want: %v, given: %v", tt.want, tt.given)
+				}
+				gaugeValue, err := storage.GaugeByName(ctx, tt.given.mId)
+				assert.NoError(t, err)
+				assert.Equalf(t, tt.want.mValue, gaugeValue, "gauge want: %v, given: %v", tt.want, tt.given)
+
+				return errForTransactionRollback
+			})
+
+			assert.Error(t, err)
+		})
+	}
+}
+func TestStorage_Gauge(t *testing.T) {
+
+	type given struct {
+		gauge map[string]float64
+	}
+	type want struct {
+		hasError bool
+		gauge    map[string]float64
+	}
+
+	tests := []struct {
+		name  string
+		given given
+		want  want
+	}{
+		{
+			name: "success map not empty",
+			given: given{
+				gauge: map[string]float64{
+					"gauge1": 100.11,
+					"gauge2": 101.11,
+				},
+			},
+			want: want{
+				hasError: false,
+				gauge: map[string]float64{
+					"gauge1": 100.11,
+					"gauge2": 101.11,
+				},
+			},
+		},
+		{
+			name: "success map is empty",
+			given: given{
+				gauge: map[string]float64{},
+			},
+			want: want{
+				hasError: false,
+				gauge:    map[string]float64{},
+			},
+		},
+	}
+
+	storage, ctx := setUpStorage(t)
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := storage.DB.RunInTransaction(ctx, func(ctx context.Context) error {
+				for mId, mValue := range tt.given.gauge {
+					err := storage.GaugeUpdate(ctx, mId, mValue)
+					if tt.want.hasError {
+						assert.Errorf(t, err, "gauge want: %v, given: %v", tt.want, tt.given)
+					} else {
+						assert.NoErrorf(t, err, "gauge want: %v, given: %v", tt.want, tt.given)
+					}
+				}
+
+				gaugeValue := storage.Gauge(ctx)
+				assert.Equalf(t, tt.want.gauge, gaugeValue, "gauge want: %v, given: %v", tt.want, tt.given)
+
+				return errForTransactionRollback
+			})
+
+			assert.Error(t, err)
+		})
+	}
+}
+
+func TestStorage_CounterAddAndCounterAccumulativeByName(t *testing.T) {
+
+	type given struct {
+		mId    string
+		mValue int64
+	}
+	type want struct {
+		hasError bool
+		mValue   int64
+	}
+
+	tests := []struct {
+		name  string
+		given given
+		want  want
+	}{
+		{
+			name: "success",
+			given: given{
+				mId:    "counter",
+				mValue: 100,
+			},
+			want: want{
+				hasError: false,
+				mValue:   100,
+			},
+		},
+	}
+
+	storage, ctx := setUpStorage(t)
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := storage.DB.RunInTransaction(ctx, func(ctx context.Context) error {
+				err := storage.CounterAdd(ctx, tt.given.mId, tt.given.mValue)
+				if tt.want.hasError {
+					assert.Errorf(t, err, "counter want: %v, given: %v", tt.want, tt.given)
+				} else {
+					assert.NoErrorf(t, err, "counter want: %v, given: %v", tt.want, tt.given)
+				}
+
+				counterValue, err := storage.CounterAccumulativeByName(ctx, tt.given.mId)
+
+				assert.NoError(t, err)
+				assert.Equalf(t, tt.want.mValue, counterValue, "counter want: %v, given: %v", tt.want, tt.given)
+
+				return errForTransactionRollback
+			})
+
+			assert.Error(t, err)
+		})
+	}
+}
+
+func TestStorage_CounterAccumulative(t *testing.T) {
+
+	type given struct {
+		counter map[string]int64
+	}
+	type want struct {
+		hasError bool
+		counter  map[string]int64
+	}
+
+	tests := []struct {
+		name  string
+		given given
+		want  want
+	}{
+		{
+			name: "success map not empty",
+			given: given{
+				counter: map[string]int64{
+					"counter1": 100,
+					"counter2": 101,
+				},
+			},
+			want: want{
+				hasError: false,
+				counter: map[string]int64{
+					"counter1": 100,
+					"counter2": 101,
+				},
+			},
+		},
+		{
+			name: "success map is empty",
+			given: given{
+				counter: map[string]int64{},
+			},
+			want: want{
+				hasError: false,
+				counter:  map[string]int64{},
+			},
+		},
+	}
+
+	storage, ctx := setUpStorage(t)
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := storage.DB.RunInTransaction(ctx, func(ctx context.Context) error {
+				for mId, mValue := range tt.given.counter {
+					err := storage.CounterAdd(ctx, mId, mValue)
+					if tt.want.hasError {
+						assert.Errorf(t, err, "counter want: %v, given: %v", tt.want, tt.given)
+					} else {
+						assert.NoErrorf(t, err, "counter want: %v, given: %v", tt.want, tt.given)
+					}
+				}
+
+				counterValue := storage.CounterAccumulative(ctx)
+				assert.Equalf(t, tt.want.counter, counterValue, "counter want: %v, given: %v", tt.want, tt.given)
+
+				return errForTransactionRollback
+			})
+
+			assert.Error(t, err)
+		})
+	}
 }
